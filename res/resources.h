c222309be9c6250d82c6cc9ca32cfe668b5285e7
DESKTOP-8550Q67\Alejandro <AlejandroRovi@gmail.com>
2026-08-11 20:28:49 +0200

    Added tech breakdown

---
+++ res/resources.h
@@ -0,0 +1,19 @@
+#include <genesis.h>
+
+#ifndef _RES_RESOURCES_H_
+#define _RES_RESOURCES_H_
+
+extern const u8 Taiko[12800];
+extern const u8 HiHat[62720];
+extern const u8 TorusMaterialData[252];
+extern const u8 TorusDiffuseMaterialData[122];
+extern const SpriteDefinition PointerSprite;
+extern const SpriteDefinition TutorialMoveLight;
+extern const SpriteDefinition TutorialToggleTexture;
+extern const SpriteDefinition TutorialCycleFeatures;
+extern const Image Torus0;
+extern const Image Torus1;
+extern const Image Title;
+extern const Image MaterialModifierUIImage;
+
+#endif // _RES_RESOURCES_H_
